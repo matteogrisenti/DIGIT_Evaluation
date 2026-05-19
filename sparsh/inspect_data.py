ad7f4112130130ee45b5354944f352aa448cd680
Matteo <grisentimatteo02@gmail.com>
2026-05-19 10:16:57 +0100

    delete old sparsh directory

--- sparsh/inspect_data.py
+++
@@ -1,5 +0,0 @@
-import pickle
-with open("../datasets/digit-force/sphere/batch_1/dataset_slip_forces.pkl", "rb") as f:
-    labels = pickle.load(f)
-first_traj_key = list(labels['trajectories'].keys())[0]
-print(f"Inside Trajectory '{first_traj_key}': {labels['trajectories'][first_traj_key].keys()}")
